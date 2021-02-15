6ffc33c6702533d74abc47d89e3811a538b041dc
Rahan <wangleibniz01@gmail.com>
2021-02-16 05:08:55 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,148 @@
+//Select
+
+const todoInput = document.querySelector(".todo-input")
+const todoButton = document.querySelector(".todo-button")
+const todoList = document.querySelector(".todo-list")
+const filterOption = document.querySelector(".filter-todo")
+
+document.addEventListener("DOMContentLoaded", getTodos)
+filterOption.addEventListener("click", filterTodo)
+todoButton.addEventListener("click", addTodo)
+todoList.addEventListener("click", deleteCheck)
+
+function addTodo(event) {
+  event.preventDefault()
+
+  const todoDiv = document.createElement("div")
+  todoDiv.classList.add("todo")
+  //create Li
+  const newTodo = document.createElement("li")
+  newTodo.innerText = todoInput.value
+  newTodo.classList.add("todo-item")
+  todoDiv.appendChild(newTodo)
+  //Save local todo
+
+  saveLocalTodos(todoInput.value)
+
+  // Completed button
+  const completedButton = document.createElement("button")
+  completedButton.innerHTML = '<i class="fas fa-check"></i>'
+  completedButton.classList.add("complete-btn")
+  todoDiv.appendChild(completedButton)
+
+  // trash button
+  const trashButton = document.createElement("button")
+  trashButton.innerHTML = '<i class="fas fa-trash"></i>'
+  trashButton.classList.add("trash-btn")
+  todoDiv.appendChild(trashButton)
+
+  todoList.appendChild(todoDiv)
+  todoInput.value = ""
+}
+
+function deleteCheck(e) {
+  const item = e.target
+  // delete
+  if (item.classList[0] === "trash-btn") {
+    const todo = item.parentElement
+    todo.classList.add("fall")
+    //animation
+    removeLocalTodos(todo)
+    todo.addEventListener("transitionend", function () {
+      todo.remove()
+    })
+  }
+
+  // check
+  if (item.classList[0] === "complete-btn") {
+    const todo = item.parentElement
+    todo.classList.toggle("completed")
+  }
+}
+
+function filterTodo(e) {
+  const todos = todoList.childNodes
+
+  todos.forEach(function (todo) {
+    switch (e.target.value) {
+      case "all":
+        todo.style.display = "flex"
+        break
+      case "completed":
+        if (todo.classList.contains("completed")) {
+          todo.style.display = "flex"
+        } else {
+          todo.style.display = "none"
+        }
+        break
+      case "uncompleted":
+        if (!todo.classList.contains("completed")) {
+          todo.style.display = "flex"
+        } else {
+          todo.style.display = "none"
+        }
+        break
+    }
+  })
+}
+
+function saveLocalTodos(todo) {
+  //check
+  let todos
+  if (localStorage.getItem("todos") === null) {
+    todos = []
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"))
+  }
+
+  todos.push(todo)
+  localStorage.setItem("todos", JSON.stringify(todos))
+}
+
+function getTodos(todo) {
+  //check
+  let todos
+  if (localStorage.getItem("todos") === null) {
+    todos = []
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"))
+  }
+
+  todos.forEach(function (todo) {
+    const todoDiv = document.createElement("div")
+    todoDiv.classList.add("todo")
+    //create Li
+    const newTodo = document.createElement("li")
+    newTodo.innerText = todo
+    newTodo.classList.add("todo-item")
+    todoDiv.appendChild(newTodo)
+
+    // Completed button
+    const completedButton = document.createElement("button")
+    completedButton.innerHTML = '<i class="fas fa-check"></i>'
+    completedButton.classList.add("complete-btn")
+    todoDiv.appendChild(completedButton)
+
+    // trash button
+    const trashButton = document.createElement("button")
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>'
+    trashButton.classList.add("trash-btn")
+    todoDiv.appendChild(trashButton)
+
+    todoList.appendChild(todoDiv)
+  })
+}
+
+function removeLocalTodos(todo) {
+  //check
+  let todos
+  if (localStorage.getItem("todos") === null) {
+    todos = []
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"))
+  }
+  const todoIndex = todo.children[0].innerText
+  todos.splice(todos.indexOf(todoIndex), 1)
+
+  localStorage.setItem("todos", JSON.stringify(todos))
+}
